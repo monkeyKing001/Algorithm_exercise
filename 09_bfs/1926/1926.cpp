eb30ec56595dd478fe76b5641c0e61c4270fbc2b
monkeyking <2018121152@yonsei.ac.kr>
2022-10-22 22:14:01 +0900

    1926 fixed

--- 09_bfs/1926/1926.cpp
+++ 09_bfs/1926/1926.cpp
@@ -19,24 +19,28 @@ int	main(int argc, char **argv)
 		for (int j = 0; j < m; j++)
 			cin >> board[i][j];
 
-	//visit start
-
-	//bfs
+	//bfs from every point
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
 			int start_row = i;
 			int start_col = j;
+			//check current coordinates can be a start point
 			if (!board[start_row][start_col] || visited[start_row][start_col])
 				continue ;
+			//bfs start
 			pair <int , int> start = make_pair(start_row, start_col);
+			//mark the start coordinate as visitied
 			visited[start_row][start_col] = 1;
+			//push the current coordinate in queue(scheduling for searching next coordinate starting from queue.front())
 			Q.push(start);
 			int	pic_size_temp = 1;
+			//pic num + 1
 			pic_num++;
 			while(!Q.empty())
 			{
+				//remove the current coordinate from the queue
 				pair <int, int> current = Q.front();
 				Q.pop();
 				for (int dir = 0; dir < 4; dir++)
@@ -48,16 +52,18 @@ int	main(int argc, char **argv)
 						continue ;
 					if (visited[next_row][next_col] || board[next_row][next_col] == 0)
 						continue ;
+					//mark the next coordinate as visited
 					visited[next_row][next_col] = 1;
+					//push the next coordinate in the queue(scheduling for searching next coordinate starting from queue.front())
 					Q.push(make_pair(next_row, next_col));
+					//pic_size plus
 					pic_size_temp++;
 				}
 			}
-			if (pic_size < pic_size_temp)
-				pic_size = pic_size_temp;
+			//update max pic size
+			pic_size = max(pic_size, pic_size_temp);
 		}
 	}
 	cout << pic_num << "\n" << pic_size;
 	return (0);
 }
-
