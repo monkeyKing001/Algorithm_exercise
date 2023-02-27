f697f0c0b48cff31da22f3568d773e6547fb2449
Dong woo Kwak <dokwak@c6r9s5.42seoul.kr>
2023-02-28 01:50:13 +0900

    v2.02

---
+++ dokwak/1C_floyd/11404/11404.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <cmath> 
+#include <algorithm>
+#include <climits>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int	main(int argc, char **argv)
+{
+	int test;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	return (0);
+}
+
