7e069a50339aa2434d78d0c9617857b1e9011ab6
Dong woo Kwak <dokwak@c6r9s3.42seoul.kr>
2023-02-28 17:07:21 +0900

    Floyd

--- dokwak/1C_floyd/11780/11780.cpp
+++ dokwak/1C_floyd/11780/11780.cpp
@@ -14,13 +14,18 @@ long long adj_mat[200][200];
 int next_path[200][200];
 int step_count[200][200];
 
-int	get_path(int v1, int v2, int &count)
+int	get_path(int v1, int v2)
 {
+	if (next_path[v1][v2] == v2)
+		cout << v2;
+	else
+		cout << next_path[v1][next_path[v1][v2]];
 	return (0);
 }
 
 int get_count(int v1, int v2)
 {
+	cout << step_count[v1][v2];
 	return (0);
 }
 
@@ -34,7 +39,6 @@ int	main(int argc, char **argv)
 		for (int j = 0; j < n; ++j)
 		{
 			adj_mat[i+1][j+1] = MAX_COST;
-			next_path[i+1][j+1] = 0;
 		}
 	}
 	for (int i = 0; i < m; ++i)
@@ -43,10 +47,8 @@ int	main(int argc, char **argv)
 		if (adj_mat[v1][v2] > cost)
 		{
 			adj_mat[v1][v2] = cost;
-			next_path[v1][v2] = v1;
+			next_path[v1][v2] = v2;
 		}
-
-		adj_mat[v1][v2] = min(cost, adj_mat[v1][v2]);
 	}
 	for (int bridge = 1; bridge <= n; ++bridge)
 	{
@@ -81,12 +83,18 @@ int	main(int argc, char **argv)
 		v1 = i+1;
 		for (int j = 0; j < n; ++j)
 		{
-			v2 = j + 1;
-			int count = 0;
-			get_count(v1, v2);
-			get_path(v1, v2, count);
+			if (i == j)
+				cout << "0\n";
+			else
+			{
+				v2 = j + 1;
+				int count = 0;
+				get_count(v1, v2);
+				cout << " ";
+				get_path(v1, v2);
+				cout << "\n";
+			}
 		}
 	}
 	return (0);
 }
-
