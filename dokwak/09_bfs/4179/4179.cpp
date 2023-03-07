688a6910519fe4342f2059f1e502bc650ef8ed1b
Dong woo Kwak <dokwak@c6r10s1.42seoul.kr>
2023-03-07 18:29:41 +0900

    4179

--- dokwak/09_bfs/4179/4179.cpp
+++ dokwak/09_bfs/4179/4179.cpp
@@ -1,3 +1,5 @@
+#include <tuple>
+#include <utility>
 #include <vector>
 #include <iostream>
 #include <cmath> 
@@ -5,15 +7,22 @@
 #include <climits>
 #include <map>
 #include <string>
+#include <stack>
+#include <queue>
 
 using namespace std;
 char maps[1001][1001];
-int j_step, f_step;
+char f_visited[1001][1001];
+char j_visited[1001][1001];
+int j_step = INT_MAX;
+int f_step = INT_MIN;
 pair <int, int> j_start;
 pair <int, int> f_start;
 int r, c;
 int dr[4] = {1, 0, -1, 0};
 int dc[4] = {0, 1, 0, -1};
+stack <tuple <int, int, int>> s;
+queue <tuple <int, int, int>> q;
 
 int	main(int argc, char **argv)
 {
@@ -48,6 +57,38 @@ int	main(int argc, char **argv)
 			cout << maps[i][j];
 		cout << "\n";
 	}
+	//bfs queue fire
+	f_step = 1;
+	q.push(make_tuple(f_start.first, f_start.second, f_step));
+	while (!q.empty())
+	{
+		int current_r, current_c, current_step;
+		current_r = get<0>(q.front());
+		current_c = get<1>(q.front());
+		current_step = get<2>(q.front());
+		q.pop();
+		f_visited[current_r][current_c] = current_step;
+		for (int i = 0; i < 4; ++i)
+		{
+			int next_r = current_r + dr[i];
+			int next_c = current_c + dc[i];
+			if (next_r > -1 && next_r < r && next_c > -1 && next_c < c)
+			{
+				if ((maps[next_r][next_c] == '.' || maps[next_r][next_c] == 'J') 
+						&& !f_visited[next_r][next_c])
+					q.push(make_tuple(next_r, next_c, current_step + 1));
+			}
+		}
+	}
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+			cout << (int)f_visited[i][j];
+		cout << "\n";
+	}
+	
+	//dfs stack jun
+
 	return (0);
 }
 
